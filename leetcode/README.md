704990104236b05a0ddb9d010f18fd2c379a421d
jungchanghae <changhae08@korea.ac.kr>
2023-11-28 23:55:10 +0900

    solve 2147

--- leetcode/README.md
+++ leetcode/README.md
@@ -6,7 +6,7 @@
 - [Maximum Number of Coins You Can Get](./maximum_number_of_coins.py) (#1561) : Medium | Array, Math, Greedy, Sorting, Game Theory (23.11.24)
 - [Sum of Absolute Differences in a Sorted Array](./sum_of_absolute_differences_in_a_sorted_array.py) (#1685) : Medium | Array, Math, Prefix Sum (23.11.25)
 - [Knight Dialer](./knight_dialer.py) (#935) : Medium | Dynamic Programming (23.11.26)
-
+- [Number of Ways to Divide a Long Corridor](./number_of_ways_to_divide_a_long_corridor.py) (#2147) : Hard | Math, String, Dynamic Programming (23.11.27)
 
 
 ## Topic
@@ -55,6 +55,7 @@
 
 - [Maximum Number of Coins You Can Get](./maximum_number_of_coins.py) (#1561) : Medium | Array, Math, Greedy, Sorting, Game Theory (23.11.24)
 - [Sum of Absolute Differences in a Sorted Array](./sum_of_absolute_differences_in_a_sorted_array.py) (#1685) : Medium | Array, Math, Prefix Sum (23.11.25)
+- [Number of Ways to Divide a Long Corridor](./number_of_ways_to_divide_a_long_corridor.py) (#2147) : Hard | Math, String, Dynamic Programming (23.11.27)
 
 </div>
 </details>
@@ -73,6 +74,16 @@
 <div markdown="1">
 
 - [Knight Dialer](./knight_dialer.py) (#935) : Medium | Dynamic Programming (23.11.26)
+- [Number of Ways to Divide a Long Corridor](./number_of_ways_to_divide_a_long_corridor.py) (#2147) : Hard | Math, String, Dynamic Programming (23.11.27)
+
+</div>
+</details>
+
+<details> 
+<summary>String</summary>
+<div markdown="1">
+
+- [Number of Ways to Divide a Long Corridor](./number_of_ways_to_divide_a_long_corridor.py) (#2147) : Hard | Math, String, Dynamic Programming (23.11.27)
 
 </div>
 </details>
---
+++ leetcode/number_of_ways_to_divide_a_long_corridor.py
@@ -0,0 +1,47 @@
+# 2147. Number of Ways to Divide a Long Corridor
+
+'''
+Along a long library corridor, there is a line of seats and decorative plants. You are given a 0-indexed string corridor of length n consisting of letters 'S' and 'P' where each 'S' represents a seat and each 'P' represents a plant.
+
+One room divider has already been installed to the left of index 0, and another to the right of index n - 1. Additional room dividers can be installed. For each position between indices i - 1 and i (1 <= i <= n - 1), at most one divider can be installed.
+
+Divide the corridor into non-overlapping sections, where each section has exactly two seats with any number of plants. There may be multiple ways to perform the division. Two ways are different if there is a position with a room divider installed in the first way but not in the second way.
+
+Return the number of ways to divide the corridor. Since the answer may be very large, return it modulo 109 + 7. If there is no way, return 0.
+'''
+
+class Solution(object):
+    def numberOfWays(self, corridor):
+        """
+        :type corridor: str
+        :rtype: int
+        """
+        if corridor.count('S') <= 1 or corridor.count('S') % 2 != 0:
+            return 0
+        elif corridor.count('S') == 2:
+            return 1
+        count = 1
+        seat_num = 0
+        prev_seat_ind = 0
+        for i, s in enumerate(corridor):
+            if s == 'S':
+                seat_num += 1 
+                if seat_num > 2 and seat_num % 2 == 1:
+                    count = count * (i-prev_seat_ind)
+                prev_seat_ind = i
+        return count % (10**9 + 7)
+        
+        # "SPS|PPSSPSSSS" 2
+        # "SPSP|PSSPSSSS"
+        # "SPSPP|SSPSSSS" 5
+        # "SPSPPSS|PSSSS"
+        # "SPSPPSSP|SSSS"
+        # "SPSPPSSPSS|SS"
+        
+        
+if __name__== '__main__':
+    sol = Solution()
+    # test Case
+    for corridor in ["SSPPSPS", "PPSPSP", "S", "SPSPPSSPSSSS"]:
+        print(sol.numberOfWays(corridor))
+        # Answer : 3 1 0
