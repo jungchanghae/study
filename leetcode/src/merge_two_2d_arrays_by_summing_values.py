6c870fb99b121426ef2eb67a773b6de59bfcf320
jungchanghae <changhae08@korea.ac.kr>
2025-03-02 20:10:42 +0900

    solved 2570 in leetcode(hash table)

--- leetcode/src/merge_two_2d_arrays_by_summing_values.py
+++ leetcode/src/merge_two_2d_arrays_by_summing_values.py
@@ -15,50 +15,85 @@ Merge the two arrays into one array that is sorted in ascending order by id, res
 
 Return the resulting array. The returned array must be sorted in ascending order by id.
 """
+from typing import List
 
 '''
-nums1의 원소들을 이용해서 하나의 리스트를 만들고,
-그 리스트에 nums2의 원소를 더하는 방식
-이후 val이 0이 아닌 것들을 반환한다.
-O(n)
+set id와 list index를 매핑하는 dict를 구성해서 필요한 index만을 사용한다.
+같은 O(n)이긴 하나, 아래보다 빠름
 '''
-from typing import List
 
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        """
-        python은 idx가 0부터 이므로 그 부분을 생각하며 작성
-        """
-        max_idx = max(self.maximum_index(nums1), self.maximum_index(nums2))
-        results_temp = []
-        results = []
         
-        for i in range(1, max_idx+1):
-            results_temp.append([i,0])
+        # id의 값들을 가지고 set을 만들기
+        set_id = set()
+        for n1_id, _ in nums1:
+            set_id.add(n1_id)
+        for n2_id, _ in nums2:
+            set_id.add(n2_id)
+        set_id = sorted(set_id)
+        
+        # id-index dict만들기
+        dict_ids = dict()
+        for idx, n_id in enumerate(set_id):
+            dict_ids[n_id] = idx
+
+        results = []        
+        for id in set_id:
+            results.append([id,0])
             
         # nums1의 값들 더하기
         for n1_id, n1_val in nums1:
-            results_temp[n1_id-1][1] += n1_val
+            results[dict_ids[n1_id]][1] += n1_val
         
         # nums2의 값들 더하기
         for n2_id, n2_val in nums2:
-            results_temp[n2_id-1][1] += n2_val
-        
-        for id, val in results_temp:
-            if val != 0:
-                results.append([id,val])
-        
-        return results
+            results[dict_ids[n2_id]][1] += n2_val
     
-    def maximum_index(self, nums: List[List[int]]) -> int:
-        """
-        각 list의 index 중 가장 큰 값을 찾는다.
-        """
-        max_idx = 0
-        for idx, val in nums:
-            if idx >= max_idx:
-                max_idx = idx
-        return max_idx
+        return results
+
+# '''
+# nums1의 원소들을 이용해서 하나의 리스트를 만들고,
+# 그 리스트에 nums2의 원소를 더하는 방식
+# 이후 val이 0이 아닌 것들을 반환한다.
+# O(n)
+# '''
+
+# class Solution:
+#     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+#         """
+#         python은 idx가 0부터 이므로 그 부분을 생각하며 작성
+#         """
+#         max_idx = max(self.maximum_index(nums1), self.maximum_index(nums2))
+#         results_temp = []
+#         results = []
+        
+#         for i in range(1, max_idx+1):
+#             results_temp.append([i,0])
+            
+#         # nums1의 값들 더하기
+#         for n1_id, n1_val in nums1:
+#             results_temp[n1_id-1][1] += n1_val
+        
+#         # nums2의 값들 더하기
+#         for n2_id, n2_val in nums2:
+#             results_temp[n2_id-1][1] += n2_val
+        
+#         for id, val in results_temp:
+#             if val != 0:
+#                 results.append([id,val])
+        
+#         return results
+    
+#     def maximum_index(self, nums: List[List[int]]) -> int:
+#         """
+#         각 list의 index 중 가장 큰 값을 찾는다.
+#         """
+#         max_idx = 0
+#         for idx, val in nums:
+#             if idx >= max_idx:
+#                 max_idx = idx
+#         return max_idx
     
 if __name__=='__main__':
     
